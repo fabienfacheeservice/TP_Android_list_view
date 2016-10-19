11cfc720ef09437e3a683707559a8fb3b21585c7
fabienfacheeservice <fabien.fache@etudiant.univ-lille1.fr>
2016-10-19 13:37:17 +0200

    update sur recyclerview layout

--- app/src/main/java/com/example/fachefabien/listviewrecyclerview/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/example/fachefabien/listviewrecyclerview/activity/RecyclerViewActivity.java
@@ -37,6 +37,34 @@ public class RecyclerViewActivity extends AppCompatActivity {
         flights.add(new Flight("FR1134", "Berlin", "Barcelone", "02/09/2017", "02/09/2017", "21:00", "23:35"));
         flights.add(new Flight("FR134", "New York", "Paris", "02/07/2017", "03/07/2017", "20:00", "23:35"));
         flights.add(new Flight("IB334", "Madrid", "Tokyo", "09/07/2017", "10/09/2017", "15:15", "17:45"));
+        flights.add(new Flight("QR42", "Paris", "Doha", "22/08/2017", "22/08/2017", "10:30", "17:55"));
+        flights.add(new Flight("TG931", "Paris", "Bangkok", "22/08/2017", "23/08/2017", "13:40", "05:55"));
+        flights.add(new Flight("EY12", "Londres", "Sydney", "19/08/2017", "20/08/2017", "09:35", "17:55"));
+        flights.add(new Flight("BA15", "Bruxelles", "Rio De Janero", "26/08/2017", "26/08/2017", "07:55", "17:15"));
+        flights.add(new Flight("FR1134", "Berlin", "Barcelone", "02/09/2017", "02/09/2017", "21:00", "23:35"));
+        flights.add(new Flight("FR134", "New York", "Paris", "02/07/2017", "03/07/2017", "20:00", "23:35"));
+        flights.add(new Flight("IB334", "Madrid", "Tokyo", "09/07/2017", "10/09/2017", "15:15", "17:45"));
+        flights.add(new Flight("QR42", "Paris", "Doha", "22/08/2017", "22/08/2017", "10:30", "17:55"));
+        flights.add(new Flight("TG931", "Paris", "Bangkok", "22/08/2017", "23/08/2017", "13:40", "05:55"));
+        flights.add(new Flight("EY12", "Londres", "Sydney", "19/08/2017", "20/08/2017", "09:35", "17:55"));
+        flights.add(new Flight("BA15", "Bruxelles", "Rio De Janero", "26/08/2017", "26/08/2017", "07:55", "17:15"));
+        flights.add(new Flight("FR1134", "Berlin", "Barcelone", "02/09/2017", "02/09/2017", "21:00", "23:35"));
+        flights.add(new Flight("FR134", "New York", "Paris", "02/07/2017", "03/07/2017", "20:00", "23:35"));
+        flights.add(new Flight("IB334", "Madrid", "Tokyo", "09/07/2017", "10/09/2017", "15:15", "17:45"));
+        flights.add(new Flight("QR42", "Paris", "Doha", "22/08/2017", "22/08/2017", "10:30", "17:55"));
+        flights.add(new Flight("TG931", "Paris", "Bangkok", "22/08/2017", "23/08/2017", "13:40", "05:55"));
+        flights.add(new Flight("EY12", "Londres", "Sydney", "19/08/2017", "20/08/2017", "09:35", "17:55"));
+        flights.add(new Flight("BA15", "Bruxelles", "Rio De Janero", "26/08/2017", "26/08/2017", "07:55", "17:15"));
+        flights.add(new Flight("FR1134", "Berlin", "Barcelone", "02/09/2017", "02/09/2017", "21:00", "23:35"));
+        flights.add(new Flight("FR134", "New York", "Paris", "02/07/2017", "03/07/2017", "20:00", "23:35"));
+        flights.add(new Flight("IB334", "Madrid", "Tokyo", "09/07/2017", "10/09/2017", "15:15", "17:45"));
+        flights.add(new Flight("QR42", "Paris", "Doha", "22/08/2017", "22/08/2017", "10:30", "17:55"));
+        flights.add(new Flight("TG931", "Paris", "Bangkok", "22/08/2017", "23/08/2017", "13:40", "05:55"));
+        flights.add(new Flight("EY12", "Londres", "Sydney", "19/08/2017", "20/08/2017", "09:35", "17:55"));
+        flights.add(new Flight("BA15", "Bruxelles", "Rio De Janero", "26/08/2017", "26/08/2017", "07:55", "17:15"));
+        flights.add(new Flight("FR1134", "Berlin", "Barcelone", "02/09/2017", "02/09/2017", "21:00", "23:35"));
+        flights.add(new Flight("FR134", "New York", "Paris", "02/07/2017", "03/07/2017", "20:00", "23:35"));
+        flights.add(new Flight("IB334", "Madrid", "Tokyo", "09/07/2017", "10/09/2017", "15:15", "17:45"));
 
         return flights;
     }
